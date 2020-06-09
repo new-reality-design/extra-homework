d4b3f499b41424f1ed4302ac34615a07116de6a7
OlgaB <olga.bereznaja@gmail.com>
2020-06-09 14:31:38 +0100

    Update cleaned the code from unwanted lines

--- script.js
+++ script.js
@@ -8,92 +8,81 @@
 через switch-case
 через многомерный массив без ифов и switch.*/
 
-let langQuestion1 = prompt('(1) Выбор языка: ru / en', 'en');
+//Условие 1
+let lang1 = prompt('(1) Выбор языка: ru / en', 'en');
 let output = document.querySelector('.output');
 
-if (langQuestion1 === 'ru') {
-  output.innerHTML = '"if-else"- Дни недели: Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье.';
-  // console.log('"if-else"- ru');
-} else if (langQuestion1 === 'en') {
-  output.innerHTML = '"if-else" - Days of the week: Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday.';
-  // console.log('"if-else" - en');
-} else if (langQuestion1 !== 'en' && langQuestion1 !== 'ru') {
-  output.innerHTML = '"if-else" : Надо выбрать язык.';
+if (lang1 === 'ru') {
+  output.innerHTML = '"1" Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье.';
+
+} else if (lang1 === 'en') {
+  output.innerHTML = '"1" Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday.';
+
+} else if (lang1 !== 'en' && lang1 !== 'ru') {
+  output.innerHTML = '"1" : Надо выбрать язык.';
 }
 
 
-let langQuestion2 = prompt('(2)- Выбор языка: ru / en', 'en');
+
+//Условие 2
+let lang2 = prompt('(2)- Выбор языка: ru / en', 'en');
 let output2 = document.querySelector('.output-2');
 
-switch (langQuestion2) {
+switch (lang2) {
   case 'ru':
-    output2.innerHTML = '"switch-case" - Дни недели: Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье.';
-    // console.log('"switch-case" - ru');
+    output2.innerHTML = '"2" Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье.';
     break;
 
   case 'en':
-    output2.innerHTML = '"switch-case" - Days of the week: Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday.';
-    // console.log('"switch-case" - en');
+    output2.innerHTML = '"2" Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday.';
     break;
 
   default:
-    output2.innerHTML = '"switch-case" : Надо выбрать язык.';
-  // console.log('???');
+    output2.innerHTML = '"2" : Надо выбрать язык.';
 }
 
 
-let langQuestion3 = prompt('(3)- Выбор языка: ru / en', 'en');
+
+//Условие 3.1
+let lang3 = prompt('(3)- Выбор языка: ru / en', 'en');
 let output3 = document.querySelector('.output-3');
 let output4 = document.querySelector('.output-4');
 let output5 = document.querySelector('.output-5');
 
 let array = [
   [
-    '"Многомерный массив" - Дни недели: ', 'Понедельник,', 'Вторник,', 'Среда,', 'Четверг,', 'Пятница,', 'Суббота,', 'Воскресенье.'
+    '"3.1"', 'Понедельник,', 'Вторник,', 'Среда,', 'Четверг,', 'Пятница,', 'Суббота,', 'Воскресенье.'
   ],
   [
-    '"Arrays" - Days of the week: ', 'Monday,', 'Tuesday,', 'Wednesday,', 'Thursday,', 'Friday,', 'Saturday,', 'Sunday.'
+    '"3.1"', 'Monday,', 'Tuesday,', 'Wednesday,', 'Thursday,', 'Friday,', 'Saturday,', 'Sunday.'
   ]
 ];
 
-langQuestion3 === 'ru' ? array[0].forEach(item => {
+lang3 === 'ru' ? array[0].forEach(item => {
   output3.innerHTML += item + ' ';
-}) : langQuestion3 === 'en' ? array[1].forEach(item => {
+}) : lang3 === 'en' ? array[1].forEach(item => {
   output4.innerHTML += item + ' ';
-}) : output5.innerHTML = '"Массивы" : Надо выбрать язык.';
+}) : output5.innerHTML = '"3.1" : Надо выбрать язык.';
 
-/*
-array.forEach(item => {
-  item.forEach(item => {
-    output3.innerHTML += item + ' ';
-  });
-  output3.innerHTML += '<br>';
-});
-*/
 
-let langQuestion4 = prompt('(4)- Выбор языка: ru / en', 'ru');
-let lang = langQuestion4;
-// let lang = 'ru';
-// let lang = 'en';
 
+
+//Условие 3.2
+let lang = prompt('(4)- Выбор языка: ru / en', 'ru');
 let arrObj = {
   ru: [
-    'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'
+    '"3.2"', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'
   ],
   en: [
-    'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'
+    '"3.2"', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'
   ]
 };
 
-console.log('Language', arrObj[lang]);
-// console.log(arrObj.en);
-
 let output6 = document.querySelector('.output-6');
 arrObj[lang].forEach(item => {
-  output6.innerHTML += item + ' ';
+  output6.innerHTML += item + '  ';
 });
-// let lang = document.querySelector('.output-6').innerHTML;
-// let lang = document.querySelector('.output-7');
+
 
 
 /*
