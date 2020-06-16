08105a4ab48220b1ad842e490799551d1766eb00
OlgaB <olga.bereznaja@gmail.com>
2020-06-16 21:12:58 +0100

    Perf finished all the tasks

--- script.js
+++ script.js
@@ -1,3 +1,60 @@
 'use strict';
 
-console.log('hello world');
+/*
+1) Создать массив arr = []
+— Записать в него 7 любых многозначных чисел в виде строк
+— Вывести в консоль только те, что начинаются с цифры 2 или 4 (Должны присутствовать в массиве)
+
+2) Вывести в столбик все простые числа от 1 до 100
+— Статья про простые числа - КЛИК
+— Рядом с каждым числом написать оба делителя данного числа
+    Например: “Делители этого числа: 1 и n”
+ */
+
+//1
+const arr = ['424', '555', '787', '111', '222', '892', '245', '909', '413', '455'];
+console.log(arr);
+
+let newArr = arr.filter(item => {
+  if (item[0] === '2' || item[0] === '4') {
+    return true;
+  }
+});
+
+console.log('newArr: ', newArr);
+
+
+//2
+// const isPrime = function (num) {
+//   if (num < 2) {
+//     return false;
+//   }
+//   for (let i = 2; i < num; i++) {
+//     if (num % i === 0) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+// for (let i = 0; i < 100; i++) {
+//   let one = '1';
+//   if (isPrime(i)) {
+//     console.log(i, 'Делители этого числа: ' + one + ' и ' + i);
+//   }
+// }
+
+// let showRes = isPrime(100);
+// console.log('showRes: ', showRes);
+
+
+//3
+const isPrimeNumber = function (num) {
+  for (let i = 2; i * i <= num; i === 2 ? i++ : i += 2) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return num > 1;
+};
+const result = [...Array(100)].reduce((a, _, i) => a.concat(isPrimeNumber(i) ? `Делители числа ${i}: 1 и ${i}` : []), []).join('\n');
+console.log(result);
